db4b7cf95c8efb6bdac11af413815d101947577c
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-12 18:15:19 +0200

    Fix scopeText and scopeStoreText

--- src/store/scope/getters.js
+++ src/store/scope/getters.js
@@ -1,15 +1,17 @@
 export function scopeText (state, getters, rootState) {
   if (state.clinic.clinics.selected.length === 1) {
-    let clinic = rootState.Model.models.clinics.items.filter(clinic => { return clinic.id === state.clinic.clinics.selected[0] })[0]
-    return clinic.nickname
+    return getters.clinicSelected.nickname
+    // let clinic = rootState.Model.models.clinics.items.filter(clinic => { return clinic.id === state.clinic.clinics.selected[0] })[0]
+    // return clinic.nickname
   } else {
     return state.clinic.clinics.selected.length + ' Clínicas'
   }
 }
 export function scopeStoreText (state, getters, rootState) {
   if (state.store.stores.selected.length === 1) {
-    let store = rootState.Model.models.stores.items.filter(store => { return store.id === state.store.stores.selected[0] })[0]
-    return store.name
+    return getters.storeSelected.name
+    // let store = rootState.Model.models.stores.items.filter(store => { return store.id === state.store.stores.selected[0] })[0]
+    // return store.name
   } else {
     return state.store.stores.selected.length + ' Locales'
   }
