1ce84504dcfc9ed1f4344f055eb80a939c56879e
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-13 13:42:28 +0200

    Add getter with the options used by the requests in every model

--- src/store/model/getters.js
+++ src/store/model/getters.js
@@ -1,4 +1,10 @@
-/*
-export function scopedModels (state) {
+export function availableOptions (state) {
+  let options = {}
+  for (let model in state.models) {
+    options[model] = {}
+    for (let option in state.models[model].options) {
+      if (state.models[model].options[option] !== false) options[model][option] = state.models[model].options[option]
+    }
+  }
+  return options
 }
-*/
