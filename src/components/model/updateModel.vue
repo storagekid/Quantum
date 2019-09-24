76e673135b94a644640e795aa2631102e9adb4e5
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-24 11:31:15 +0200

    Remove commented lines

--- src/components/model/updateModel.vue
+++ src/components/model/updateModel.vue
@@ -17,14 +17,7 @@
         v-on:loaded="visible = false"
       >
       </update-model-tabs>
-      <!-- <generic-data-loading v-else></generic-data-loading> -->
     </q-card-section>
-    <!-- <multi-upload-bars
-      v-if="multiUpload.show"
-      :opened="multiUpload.show"
-      :items="multiUpload.items"
-      >
-    </multi-upload-bars> -->
     <q-inner-loading :showing="visible">
       <q-spinner-gears size="50px" color="primary"></q-spinner-gears>
     </q-inner-loading>
@@ -33,53 +26,21 @@
 
 <script>
 import UpdateModelTabs from '../model/updateModelTabs'
-// import { ModelsFetcher } from '../../mixins/modelMixin'
 
 export default {
   name: 'UpdateModel',
   components: { UpdateModelTabs },
   props: ['modelName', 'model', 'batchMode', 'modelsNeeded', 'quasarData'],
-  // mixins: [ModelsFetcher],
   data () {
     return {
       visible: false
-      // multiUpload: {
-      //   show: false,
-      //   items: []
-      // }
     }
   },
   methods: {
-    // showMultiUpload (payload) {
-    //   this.multiUpload.show = true
-    //   for (let item of payload.items) {
-    //     this.$set(item, 'state', 'waiting')
-    //   }
-    //   payload.items[0].state = 'uploading'
-    //   this.multiUpload.items = payload.items
-    // },
-    // multiUploadFileSuccess (payload) {
-    //   this.multiUpload.items[payload.index].state = 'success'
-    //   if (payload.index < this.multiUpload.items.length - 1) {
-    //     this.multiUpload.items[payload.index + 1].state = 'uploading'
-    //   }
-    // },
-    // multiUploadFileFailed (payload) {
-    //   this.multiUpload.items[payload.index].done = 'failed'
-    //   if (payload.index < this.multiUpload.items.length - 1) {
-    //     this.multiUpload.items[payload.index + 1].state = 'uploading'
-    //   }
-    // },
-    // clearMultiUpload () {
-    //   this.multiUpload.show = false
-    //   this.multiUpload.items = []
-    // },
     changeStatus () {
       this.$q.loading.show()
-      // this.visible = true
     },
     closeModal () {
-      // this.visible = false
       this.$q.loading.hide()
       this.$emit('modelUpdated')
     }
