a35fab904c60c1b4786c873e706b0000c87385c0
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-25 16:15:19 +0200

    ClinicScopeSelector refactoring

--- src/components/scope/clinicSelector.vue
+++ src/components/scope/clinicSelector.vue
@@ -22,12 +22,11 @@
           multiple
           use-input
           input-debounce="0"
-          @filter="filterState"
           :display-value="statesText"
           v-model="stateSelected"
           label="Selecciona una CCAA"
           stack-label
-          :options="stateOptions"
+          :options="states"
         >
         <template v-slot:append>
           <q-icon name="clear" @click.stop="stateSelected = []" />
@@ -37,12 +36,11 @@
           multiple
           use-input
           input-debounce="0"
-          @filter="filterCounty"
           :display-value="countiesText"
           v-model="countySelected"
           label="Selecciona una Provincia"
           stack-label
-          :options="countyOptions"
+          :options="counties"
         >
         <template v-slot:append>
           <q-icon name="clear" @click.stop="countySelected = []" />
@@ -52,12 +50,11 @@
           multiple
           use-input
           input-debounce="0"
-          @filter="filterClinic"
           :display-value="clinicsText"
           v-model="clinicSelected"
           label="Selecciona una Clínica"
           stack-label
-          :options="clinicOptions"
+          :options="clinics"
         >
         <template v-slot:append>
           <q-icon name="clear" @click.stop="clinicSelected = []" />
@@ -81,147 +78,80 @@ export default {
       clinicSelected: [],
       countrySelected: [],
       countySelected: [],
-      stateSelected: [],
-      stateOptions: null,
-      countyOptions: null,
-      clinicOptions: null
+      stateSelected: []
+    }
+  },
+  watch: {
+    countriesIDs () {
+      this.stateSelected = this.stateSelected.filter(i => { return this.countriesIDs.includes(i.country_id) })
+    },
+    statesIDs () {
+      this.countySelected = this.countySelected.filter(i => { return this.statesIDs.includes(i.state_id) })
+    },
+    countiesIDs () {
+      this.clinicSelected = this.clinicSelected.filter(i => { return this.countiesIDs.includes(i.county_id) })
     }
   },
   computed: {
     clinicsText () {
-      return `${this.clinicSelected.length > 0 ? this.clinicSelected.length : this.clinics.length} ${this.clinicSelected.length > 1 || !this.clinicSelected.length ? 'seleccionadas' : 'seleccionada'}`
+      return `${this.clinicSelected.length} ${this.clinicSelected.length > 1 || !this.clinicSelected.length ? 'seleccionadas' : 'seleccionada'}`
     },
     countiesText () {
-      return `${this.countySelected.length > 0 ? this.countySelected.length : this.counties.length} ${this.countySelected.length > 1 || !this.countySelected.length ? 'seleccionadas' : 'seleccionada'}`
+      return `${this.countySelected.length} ${this.countySelected.length > 1 || !this.countySelected.length ? 'seleccionadas' : 'seleccionada'}`
     },
     statesText () {
-      return `${this.stateSelected.length > 0 ? this.stateSelected.length : this.states.length} ${this.stateSelected.length > 1 || !this.stateSelected.length ? 'seleccionadas' : 'seleccionada'}`
+      return `${this.stateSelected.length} ${this.stateSelected.length > 1 || !this.stateSelected.length ? 'seleccionadas' : 'seleccionada'}`
     },
     countriesText () {
-      return `${this.countrySelected.length > 0 ? this.countrySelected.length : this.countries.length} ${this.countrySelected.length > 1 || !this.countrySelected.length ? 'seleccionados' : 'seleccionado'}`
+      return `${this.countrySelected.length} ${this.countrySelected.length > 1 || !this.countrySelected.length ? 'seleccionados' : 'seleccionado'}`
     },
     countriesIDs () {
-      let ids = []
-      this.countrySelected.map(i => { ids.push(i.id) })
-      return ids
+      return this.countrySelected.map(i => { return i.id })
     },
     statesIDs () {
-      let ids = []
-      this.stateSelected.map(i => { ids.push(i.id) })
-      return ids
+      return this.stateSelected.map(i => { return i.id })
     },
     countiesIDs () {
-      let ids = []
-      this.countySelected.map(i => { ids.push(i.id) })
-      return ids
+      return this.countySelected.map(i => { return i.id })
     },
     countries () {
-      let countries = this.$store.state.Model.models.countries.items
-      // let countries = this.countrySelected.length ? this.countrySelected : this.$store.state.Scope.clinic.countries.items
-      return countries
+      return this.$store.state.Model.models.countries.items
     },
     states () {
-      let states = this.$store.state.Model.models.states.items
-      // let states = this.stateSelected.length ? this.stateSelected : this.$store.state.Scope.clinic.states.items
-      if (this.countrySelected.length) {
-        let vm = this
-        states = states.filter(state => vm.countriesIDs.indexOf(state.country_id) !== -1)
-      }
-      return states
+      return this.$store.state.Model.models.states.items.filter(i => { return this.countriesIDs.includes(i.country_id) })
     },
     counties () {
-      let counties = this.$store.state.Model.models.counties.items
-      // let counties = this.countySelected.length ? this.countySelected : this.$store.state.Scope.clinic.counties.items
-      if (this.stateSelected.length) {
-        let vm = this
-        counties = counties.filter(county => vm.statesIDs.indexOf(county.state_id) !== -1)
-      } else if (this.countrySelected.length) {
-        let vm = this
-        counties = counties.filter(county => vm.countriesIDs.indexOf(county.state.country_id) !== -1)
-      }
-      return counties
+      return this.$store.state.Model.models.counties.items.filter(i => { return this.statesIDs.includes(i.state_id) })
     },
     clinics () {
-      let clinics = this.$store.state.Model.models.clinics.items
-      // let clinics = this.clinicSelected.length ? this.clinicSelected : this.$store.state.Scope.clinic.clinics.items
-      if (this.countySelected.length) {
-        let vm = this
-        clinics = clinics.filter(clinic => vm.countiesIDs.indexOf(clinic.county_id) !== -1)
-      } else if (this.stateSelected.length) {
-        let vm = this
-        clinics = clinics.filter(clinic => vm.statesIDs.indexOf(clinic.county.state_id) !== -1)
-      } else if (this.countrySelected.length) {
-        let vm = this
-        clinics = clinics.filter(clinic => vm.countriesIDs.indexOf(clinic.county.state.country_id) !== -1)
-      }
-      return clinics
+      return this.$store.state.Model.models.clinics.items.filter(i => { return this.countiesIDs.includes(i.county_id) })
     }
   },
   methods: {
-    filterState (val, update) {
-      if (val === '') {
-        update(() => {
-          this.stateOptions = this.states
-        })
-        return
-      }
-      update(() => {
-        const needle = val.toLowerCase()
-        this.stateOptions = this.states.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
-      })
-    },
-    filterCounty (val, update) {
-      if (val === '') {
-        update(() => {
-          this.countyOptions = this.counties
-        })
-        return
-      }
-      update(() => {
-        const needle = val.toLowerCase()
-        this.countyOptions = this.counties.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
-      })
-    },
-    filterClinic (val, update) {
-      if (val === '') {
-        update(() => {
-          this.clinicOptions = this.clinics
-        })
-        return
-      }
-      update(() => {
-        const needle = val.toLowerCase()
-        this.clinicOptions = this.clinics.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
-      })
-    },
     setScope () {
       let countries = []
       let states = []
       let counties = []
       let clinics = []
       if (this.countrySelected.length) {
-        // countries = this.countrySelected
-        this.countrySelected.map(i => { countries.push(i.id) })
+        countries = this.countrySelected
       } else {
-        this.countries.forEach(country => { countries.push(country.id) }, countries)
+        countries = this.countries
       }
       if (this.stateSelected.length) {
-        // states = this.stateSelected
-        this.stateSelected.map(i => { states.push(i.id) })
+        states = this.stateSelected
       } else {
-        this.states.forEach(state => { if (countries.indexOf(state.country_id) !== -1) { states.push(state.id) } }, countries, states)
+        states = this.states
       }
       if (this.countySelected.length) {
-        // counties = this.countySelected
-        this.countySelected.map(i => { counties.push(i.id) })
+        counties = this.countySelected
       } else {
-        this.counties.forEach(county => { if (states.indexOf(county.state_id) !== -1) { counties.push(county.id) } }, states, counties)
+        counties = this.counties
       }
       if (this.clinicSelected.length) {
-        // clinics = this.clinicSelected
-        this.clinicSelected.map(i => { clinics.push(i.id) })
+        clinics = this.clinicSelected
       } else {
-        this.clinics.forEach(clinic => { if (counties.indexOf(clinic.county_id) !== -1) { clinics.push(clinic.id) } }, counties, clinics)
+        clinics = this.clinics
       }
       let scope = { countries: countries, states: states, counties: counties, clinics: clinics }
       if (this.$store.state.Scope.mode !== 'clinic') this.$store.commit('Scope/setScopeMode', 'clinic')
@@ -244,26 +174,17 @@ export default {
     },
     initialScope () {
       if (this.$store.state.Scope.clinic.clinics.selected.length) {
-        this.clinicSelected = this.$store.state.Scope.clinic.clinics.items.filter(i => { return this.$store.state.Scope.clinic.clinics.selected.includes(i.id) })
+        this.clinicSelected = this.$store.state.Scope.clinic.clinics.selected
       }
       if (this.$store.state.Scope.clinic.counties.selected.length) {
-        this.countySelected = this.$store.state.Scope.clinic.counties.items.filter(i => { return this.$store.state.Scope.clinic.counties.selected.includes(i.id) })
+        this.countySelected = this.$store.state.Scope.clinic.counties.selected
       }
       if (this.$store.state.Scope.clinic.states.selected.length) {
-        this.stateSelected = this.$store.state.Scope.clinic.states.items.filter(i => { return this.$store.state.Scope.clinic.states.selected.includes(i.id) })
+        this.stateSelected = this.$store.state.Scope.clinic.states.selected
       }
       if (this.$store.state.Scope.clinic.countries.selected.length) {
-        this.countrySelected = this.$store.state.Scope.clinic.countries.items.filter(i => { return this.$store.state.Scope.clinic.countries.selected.includes(i.id) })
+        this.countrySelected = this.$store.state.Scope.clinic.countries.selected
       }
-      // if (this.$store.state.Scope.clinic.counties.selected) {
-      //   this.countySelected = this.$store.state.Scope.clinic.counties.selected
-      // }
-      // if (this.$store.state.Scope.clinic.states.selected) {
-      //   this.stateSelected = this.$store.state.Scope.clinic.states.selected
-      // }
-      // if (this.$store.state.Scope.clinic.countries.selected) {
-      //   this.countrySelected = this.$store.state.Scope.clinic.countries.selected
-      // }
     }
   },
   created () {
