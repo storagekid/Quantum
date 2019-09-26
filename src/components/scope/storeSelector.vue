5e5ec6fe5a2cb6765511199d89308ce422b61b82
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-26 10:41:12 +0200

    Rebuild StoreSelecto based on ClinicSelector

--- src/components/scope/storeSelector.vue
+++ src/components/scope/storeSelector.vue
@@ -6,27 +6,29 @@
     <q-separator />
     <q-card-section>
       <form>
-        <q-select
-          filter
-          multiple
-          chips
-          clearable
-          :display-value="countriesText"
-          v-model="countrySelected"
-          float-label="Selecciona un País"
-          :options="countries"
-        />
-        <q-select
-          filter
-          multiple
-          chips
-          clearable
-          :display-value="storesText"
-          v-model="storeSelected"
-          float-label="Selecciona un Local"
-          :options="stores"
-        />
-        <q-btn color="primary" label="Aceptar" class="q-mt-md full-width" @click="setStoreScope"/>
+        <custom-select
+          :dense="true"
+          :multiple="true"
+          :clearable="true"
+          :hide-bottom-space="true"
+          :field="{name: 'countries', type: { model: 'countries', default: { text: 'Selecciona un País'} }}"
+          :sourceOptions="countries"
+          :initValue="countrySelected"
+          @updated="updateCustomSelect('countrySelected', $event)"
+          >
+        </custom-select>
+        <custom-select
+          :dense="true"
+          :clearable="true"
+          :multiple="true"
+          :hide-bottom-space="true"
+          :field="{name: 'stores', type: { model: 'stores', default: { text: 'Selecciona una Clínica'} }}"
+          :sourceOptions="stores"
+          :initValue="storeSelected"
+          @updated="updateCustomSelect('storeSelected', $event)"
+          >
+        </custom-select>
+        <q-btn color="primary" label="Aceptar" class="q-mt-md full-width" @click="setScope"/>
       </form>
     </q-card-section>
     <q-inner-loading :showing="visible">
@@ -36,8 +38,13 @@
 </template>
 
 <script>
+import CustomSelect from '../form/customSelect'
+import { Helpers } from '../../mixins/helpers'
+
 export default {
-  // name: 'ComponentName',
+  name: 'StoreSelector',
+  mixins: [Helpers],
+  components: { CustomSelect },
   data () {
     return {
       visible: false,
@@ -45,39 +52,41 @@ export default {
       storeSelected: []
     }
   },
+  watch: {
+    countriesIDs () {
+      this.storeSelected = this.storeSelected.filter(i => { return this.countriesIDs.includes(i.country_id) })
+    }
+  },
   computed: {
     countriesText () {
-      return `${this.countrySelected.length > 0 ? this.countrySelected.length : this.countries.length} ${this.countrySelected.length > 1 || !this.countrySelected.length ? 'seleccionados' : 'seleccionado'}`
+      return `${this.countrySelected.length} ${this.countrySelected.length > 1 || !this.countrySelected.length ? 'seleccionados' : 'seleccionado'}`
     },
-    storesText () {
-      return `${this.storeSelected.length > 0 ? this.storeSelected.length : this.stores.length} ${this.storeSelected.length > 1 || !this.storeSelected.length ? 'seleccionados' : 'seleccionado'}`
+    clinicsText () {
+      return `${this.storeSelected.length} ${this.storeSelected.length > 1 || !this.storeSelected.length ? 'seleccionadas' : 'seleccionada'}`
+    },
+    countriesIDs () {
+      return this.countrySelected.map(i => { return i.id })
     },
     countries () {
-      let countries = this.$store.state.Scope.store.countries.items
-      return countries
+      return this.$store.state.Model.models.countries.items
     },
     stores () {
-      let stores = this.$store.state.Scope.store.stores.items
-      if (this.countrySelected.length) {
-        let vm = this
-        stores = stores.filter(store => vm.countrySelected.indexOf(store.country_id) !== -1)
-      }
-      return stores
+      return this.$store.state.Model.models.stores.items.filter(i => { return this.countriesIDs.includes(i.country_id) })
     }
   },
   methods: {
-    setStoreScope () {
+    setScope () {
       let countries = []
       let stores = []
       if (this.countrySelected.length) {
         countries = this.countrySelected
       } else {
-        this.countries.forEach(country => { countries.push(country.id) }, countries)
+        countries = this.countries
       }
       if (this.storeSelected.length) {
         stores = this.storeSelected
       } else {
-        this.stores.forEach(store => { if (countries.indexOf(store.country_id) !== -1) { stores.push(store.id) } }, countries, stores)
+        stores = this.stores
       }
       let scope = { countries: countries, stores: stores }
       if (this.$store.state.Scope.mode !== 'store') this.$store.commit('Scope/setScopeMode', 'store')
@@ -89,20 +98,38 @@ export default {
         .then(() => {
           this.$q.loading.hide()
           this.$emit('StoreScopeSelected')
+        }).catch(() => {
+          this.$store.dispatch('Model/cleanScopedModels', scope).then(() => {
+            this.$store.dispatch('Notify/displayMessage', { message: 'Too Many Stores Selected', position: 'top', type: 'warning' })
+            this.$store.dispatch('Notify/displayMessage', { message: 'Please select less than 10 stores to retrieve models related', position: 'top', type: 'info' })
+          })
+          this.$q.loading.hide()
+          this.$emit('StoreScopeSelected')
         })
     },
     initialScope () {
-      if (this.$store.state.Scope.store.stores.selected) {
-        this.storeSelected = this.$store.state.Scope.store.stores.selected
-      }
-      if (this.$store.state.Scope.store.countries.selected) {
+      // console.log('initialScope')
+      if (this.$store.state.Scope.store.countries.selected.length) {
         this.countrySelected = this.$store.state.Scope.store.countries.selected
+      }
+      if (this.$store.state.Scope.store.stores.selected.length) {
+        // console.log('Clinics.Selected')
+        this.storeSelected = this.$store.state.Scope.store.stores.selected
       }
     }
   },
   created () {
-  },
-  mounted () {
+    // console.log(this.$router.currentRoute.name)
+    if (this.$store.state.Scope.store.stores.selected.length === 1 && this.$router.currentRoute.name === 'login') {
+      // console.log('1 Clinic')
+      if (localStorage.lastPage) {
+        this.$router.push({
+          name: localStorage.lastPage
+        })
+      } else {
+        this.$router.push({ name: this.$store.state.App.homePage })
+      }
+    }
     this.initialScope()
   }
 }
