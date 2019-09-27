74081355b88520d1d8bf1744906e2789deac3c0f
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-27 10:57:20 +0200

    Add data and methods spread across components and mixins+

--- src/mixins/customSelectMixins.js
+++ src/mixins/customSelectMixins.js
@@ -1,4 +1,9 @@
 export const customSelectMixins = {
+  data () {
+    return {
+      CustomSelectOriginalTypes: {}
+    }
+  },
   methods: {
     hideDatePicker (index) {
       let picker = 'qDateProxy-' + index
@@ -13,6 +18,38 @@ export const customSelectMixins = {
       }
       ids.push(this.model.id)
       return ids
+    },
+    updateCustomSelect (object, payload, onRelation = false) {
+      if (onRelation) {
+        if (this.relationData.type === 'BelongsToMany') {
+          console.log('BelongsToMany CustomSelect')
+          this.relation = payload
+          this.$v.relation.$touch()
+        } else {
+          this.relation[object] = payload
+          this.$v.relation[object].$touch()
+        }
+      }
+      if (object.indexOf('.') > -1) {
+        let words = object.split('.')
+        let mainObject = this[words[0]]
+        for (let i = 1; i < words.length - 1; i++) {
+          mainObject = mainObject[words[i]]
+        }
+        if (typeof this.CustomSelectOriginalTypes[object] === 'undefined') {
+          this.CustomSelectOriginalTypes[object] = this.findOriginalObjectType(mainObject[words[words.length - 1]])
+        }
+        mainObject[words[words.length - 1]] = payload !== null ? payload : this.CustomSelectOriginalTypes[object]
+      } else {
+        if (typeof this.CustomSelectOriginalTypes[object] === 'undefined') {
+          this.CustomSelectOriginalTypes[object] = this.findOriginalObjectType(this[object])
+        }
+        this[object] = payload !== null ? payload : this.CustomSelectOriginalTypes[object]
+      }
+    },
+    findOriginalObjectType (object) {
+      // console.log(object)
+      return (object === null) ? null : Array.isArray(object) ? [] : (typeof object === 'object') ? {} : (typeof object === 'string') ? '' : false
     }
   }
 }
