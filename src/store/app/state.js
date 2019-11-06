07aedbf457bda14cd295462a3852fe593dd595e1
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-06 09:15:54 +0100

    Add Production Server Attribute

--- src/store/app/state.js
+++ src/store/app/state.js
@@ -1,6 +1,7 @@
 export default {
   // dataWarehouse: 'http://192.168.3.234:2200/api/rest/', // DENTIX WIRED
   dataWarehouse: 'http://migabinete.test/api/rest/', // DENTIX WIRED 2
+  // dataWarehouse: 'http://dentix-quantum.test:8100/api/rest/', // IMAC 21
   serverAddress: 'http://migabinete.test',
   publicSources: 'http://migabinete.test/storage',
   // dataWarehouse: 'http://192.168.1.179:2200/api/rest/', // HOME WiFi
