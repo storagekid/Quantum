5120090d47d50939ab749b0acab34bb29e4933ed
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-25 09:41:29 +0100

    Picture now takes you to profile page

--- src/components/profile/profileCard.vue
+++ src/components/profile/profileCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <q-img :src="require('../../assets/profile.png')">
-    <div class="absolute-bottom text-subtitle1 text-center q-pa-xs">
-      {{$store.state.User.profile.full_name}}
-      <div class="text-caption">{{$store.state.User.profile.job_type_name}}</div>
-    </div>
-  </q-img>
+  <router-link to="/profile-home">
+    <q-img :src="require('../../assets/profile.png')">
+      <div class="absolute-bottom text-subtitle1 text-center q-pa-xs">
+        {{$store.state.User.profile.full_name}}
+        <div class="text-caption">{{$store.state.User.profile.job_type_name}}</div>
+      </div>
+    </q-img>
+  </router-link>
 </template>
 
 <script>
