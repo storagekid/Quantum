45c2ce3ab40feb0319282385d9da33bf0315e3c5
JG Villalba <jgvillalba@mozodealmacen.com>
2019-12-16 10:04:31 +0100

    New Scope Getter to Sort Clinics By Scope

--- src/store/scope/getters.js
+++ src/store/scope/getters.js
@@ -22,3 +22,17 @@ export function storeSelected (state, getters, rootState) {
     return state.store.stores.selected[0]
   }
 }
+
+export function clinicsByScope (state) {
+  if (!state.clinic.clinics.items.length || !state.setted) return null
+  else {
+    let modelsByScope = {}
+    for (let item of state.clinic.clinics.items) {
+      if (!modelsByScope[item.county.state.country_id]) modelsByScope[item.county.state.country_id] = {}
+      if (!modelsByScope[item.county.state.country_id][item.county.state_id]) modelsByScope[item.county.state.country_id][item.county.state_id] = {}
+      if (!modelsByScope[item.county.state.country_id][item.county.state_id][item.county_id]) modelsByScope[item.county.state.country_id][item.county.state_id][item.county_id] = []
+      modelsByScope[item.county.state.country_id][item.county.state_id][item.county_id].push(item)
+    }
+    return modelsByScope
+  }
+}
