8192a6e94a19a7f36ba72b660cb4b7e51884b5e8
JG Villalba <jgvillalba@mozodealmacen.com>
2019-12-16 15:08:26 +0100

    Check for Data in response Manager

--- src/store/response/actions.js
+++ src/store/response/actions.js
@@ -1,16 +1,18 @@
 export function responseErrorManager ({ commit, dispatch }, response) {
-  if (response.data.exception) {
-    if (response.status) dispatch('responseStatusPrinter', response)
-    else dispatch('Notify/displayMessage', { message: 'Error en el servidor. Por favor contacte con el admimistrador.', position: 'top', type: 'negative' }, { root: true })
-    return
-  }
-  if (response.data.errors) {
-    for (let error in response.data.errors) {
-      dispatch('Notify/displayMessage', { message: response.data.errors[error][0], position: 'top', type: 'negative' }, { root: true })
+  if (response.data) {
+    if (response.data.exception) {
+      if (response.status) dispatch('responseStatusPrinter', response)
+      else dispatch('Notify/displayMessage', { message: 'Error en el servidor. Por favor contacte con el admimistrador.', position: 'top', type: 'negative' }, { root: true })
+      return
     }
-  }
-  if (response.data.message) {
-    dispatch('Notify/displayMessage', { message: response.data.message, position: 'top', type: 'negative' }, { root: true })
+    if (response.data.errors) {
+      for (let error in response.data.errors) {
+        dispatch('Notify/displayMessage', { message: response.data.errors[error][0], position: 'top', type: 'negative' }, { root: true })
+      }
+    }
+    if (response.data.message) {
+      dispatch('Notify/displayMessage', { message: response.data.message, position: 'top', type: 'negative' }, { root: true })
+    }
   }
   if (!response.status) {
     dispatch('Notify/displayMessage', { message: 'Estamos en mantenimiento. Disculpe las molestias', position: 'top', type: 'negative' }, { root: true })
