0ed7ecad7705b83227db455363435e859d216940
JG Villalba <jgvillalba@mozodealmacen.com>
2020-01-10 12:58:44 +0100

    Add helper to fetch dateString

--- src/mixins/helpers.js
+++ src/mixins/helpers.js
@@ -8,6 +8,17 @@ export const Helpers = {
     },
     log (params) {
       this.$store.commit('App/log', params)
+    },
+    dateString (option = null, date = new Date(Date.now())) {
+      // console.log('Getting Date')
+      let year = date.getFullYear()
+      let month = (date.getMonth() < 10 ? '0' : '') + (Number(date.getMonth()) + 1)
+      let day = null
+      if (option === 'first') day = new Date(year, date.getMonth(), 1).getDate()
+      else if (option === 'last') day = new Date(year, date.getMonth() + 1, 0).getDate()
+      else day = date.getDate()
+      let fullday = (day < 10 ? '0' : '') + day
+      return year + '-' + month + '-' + fullday
     }
   }
 }
